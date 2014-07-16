a851523ba44afd265dcc6f72d500f7690e2f26e5
Jannes <jannes.meyer@gmail.com>
2014-07-16 11:04:51 +0200

    Style change

--- src/tab.js
+++ src/tab.js
@@ -1,3 +1,9 @@
+var clock = {
+	hours: document.querySelector('.m-clock > .hours').firstChild,
+	minutes: document.querySelector('.m-clock > .minutes').firstChild
+};
+render();
+
 function padZero(number) {
 	var str = number.toString();
 	return str.length < 2 ? '0' + str : str;
@@ -11,17 +17,10 @@ function render() {
 
 	// var shift = -7;
 	// var dst = +1;
-	// var hours = (24 + now.getUTCHours() + shift + dst) % 24;
+	// var hours = (now.getUTCHours() + shift + dst + 24) % 24;
 
 	clock.hours.data = hours;
 	clock.minutes.data = padZero(minutes);
 
 	setTimeout(render, (60 - seconds) * 1000);
 }
-
-var clock = {
-	hours: document.querySelector('.m-clock > .hours').firstChild,
-	minutes: document.querySelector('.m-clock > .minutes').firstChild
-};
-
-render();
